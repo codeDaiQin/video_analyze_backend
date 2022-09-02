8b1047544731afa2482e9a7443b16b07cfee42e5
daiqin <daiqin@laiye.com>
2022-09-02 18:07:13 +0800

    feat: 🐛 init project

--- src/common/errorHandle.ts
+++ src/common/errorHandle.ts
@@ -3,7 +3,6 @@ import {
   Catch,
   ExceptionFilter,
   HttpStatus,
-  ServiceUnavailableException,
 } from '@nestjs/common';
 import { FastifyReply, FastifyRequest } from 'fastify';
 
@@ -14,14 +13,14 @@ export class HTTPErrorHandle implements ExceptionFilter {
     const response = ctx.getResponse<FastifyReply>();
     const request = ctx.getRequest<FastifyRequest>();
 
-    request.log.error(exception);
+    request.log.error(exception, 'error ---- ');
 
     // 非 HTTP 标准异常的处理。
     response.status(HttpStatus.SERVICE_UNAVAILABLE).send({
       statusCode: HttpStatus.SERVICE_UNAVAILABLE,
       timestamp: new Date().toISOString(),
       path: request.url,
-      message: new ServiceUnavailableException().getResponse(),
+      message: exception.message,
     });
   }
 }
