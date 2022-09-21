136d43ba655a54ad9590fbdde27db520f87dc7bc
daiqin <daiqin@laiye.com>
2022-09-21 11:57:31 +0800

    feat: 🐛 add code login method

--- src/modules/auth/dto/auth-register.dto.ts
+++ src/modules/auth/dto/auth-register.dto.ts
@@ -1,34 +1,20 @@
 import { ApiProperty } from '@nestjs/swagger';
-import {
-  validate,
-  validateOrReject,
-  Contains,
-  IsInt,
-  Length,
-  IsEmail,
-  IsFQDN,
-  IsDate,
-  Min,
-  Max,
-  IsNotEmpty,
-  IsString,
-} from 'class-validator';
+import { Length, IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
-// Data Transfer Object（数据传输对象）
 export class RegisterDto {
+  @IsNotEmpty({ message: '邮箱不能为空' })
+  @IsString()
+  @IsEmail({}, { message: '邮箱格式不正确' })
   @ApiProperty({
     description: '邮箱',
   })
-  @IsString()
-  @IsEmail()
-  @IsNotEmpty()
   email: string;
 
+  @IsNotEmpty({ message: '验证码不能为空' })
+  @IsString()
+  @Length(4, 4)
   @ApiProperty({
     description: '验证码',
   })
-  @IsString()
-  @Length(4, 4)
-  @IsNotEmpty()
   code: string;
 }
